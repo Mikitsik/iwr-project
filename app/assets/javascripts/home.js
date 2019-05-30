40be312a3e8e21862cdb9d5bfdee83234c876194
Aleks <mikitsik@gmail.com>
2019-05-30 14:10:23 +0300

    Add some changes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,9 +1,11 @@
-$("#getstarted").click(function() {
+function login() {
   $("#loginlay").removeClass("d-none");
   $("#loginlayunder").removeClass("d-none");
-});
+};
 
 $("#loginlay").click(function() {
   $(this).addClass("d-none");
   $("#loginlayunder").addClass("d-none");
 });
+
+function vacation() {};
