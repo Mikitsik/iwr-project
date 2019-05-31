18557d8f1494d341f000e7f5da8b11b7b2e09fc2
Aleks <mikitsik@gmail.com>
2019-05-31 16:53:14 +0300

    Remove calling JS from HTML

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,11 +1,9 @@
-function login() {
+$("#getstarted").click(function() {
   $("#loginlay").removeClass("d-none");
   $("#loginlayunder").removeClass("d-none");
-};
+});
 
 $("#loginlay").click(function() {
   $(this).addClass("d-none");
   $("#loginlayunder").addClass("d-none");
 });
-
-function vacation() {};
