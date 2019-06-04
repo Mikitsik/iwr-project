38c0c04954fcda383f494806c7f91fbba7ef3b23
Aleks <mikitsik@gmail.com>
2019-06-04 12:51:13 +0300

    Technical commit

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,19 @@
 // open-close modal window
+var userName = document.querySelector("#user_name"),
+    sessionEmail = document.querySelector("#session_email"),
+    userEmail = document.querySelector("#user_email"),
+    regExpEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    redAlert = function() {
+      userEmail.style.border = "solid red 2px";
+      $( "#goodnote" ).addClass( "d-none" );
+      $( "#badnote" ).removeClass( "d-none" );
+    },
+    greenAlert = function() {
+      userEmail.style.border = "solid green 2px";
+      $( "#badnote" ).addClass( "d-none" );
+      $( "#goodnote" ).removeClass( "d-none" );
+    };
+
 $( "#getstarted" ).click( function() {
   $( "#loginlay" ).removeClass( "d-none" );
   $( "#loginlayunder" ).removeClass( "d-none" );
@@ -14,6 +29,7 @@ $( "#register" ).click( function() {
   $( "#loginlayunder" ).addClass( "d-none" );
   $( "#registerlay" ).removeClass( "d-none" );
   $( "#registerlayunder" ).removeClass( "d-none" );
+  userName.focus();
 });
 
 $( "#registerlay" ).click( function() {
@@ -26,5 +42,25 @@ $( "#loginback" ).click( function() {
   $( "#registerlayunder" ).addClass( "d-none" );
   $( "#loginlay" ).removeClass( "d-none" );
   $( "#loginlayunder" ).removeClass( "d-none" );
+  sessionEmail.focus();
 })
 
+userEmail.addEventListener( "keyup", function() {
+  if ( regExpEmail.test( userEmail.value )) {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        if (this.responseText) {
+              greenAlert();
+            } else {
+              redAlert();
+            }
+          }
+        };
+      xmlhttp.open("GET", "gethint.php?q=" + str, true);
+      xmlhttp.send();
+  } else {
+    redAlert();
+  }
+});
