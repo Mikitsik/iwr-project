3badd4db67cb1acb945bed5879c96f466be5c3d7
Aleks <mikitsik@gmail.com>
2019-06-25 12:36:28 +0300

    TK3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 
 class UsersController < ApplicationController
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_params).call
     if @user.save
       @user.send_activation_email
       flash[:info] = "YaY! Check your mailbox to proceed! ;-)"
